91e4ea0eb0ea4e2f70c05eccf821d5134d266e9e
youking-lib <whistleyz@163.com>
2024-02-20 12:02:12 +0800

    feat(desktop): init

---
+++ apps/desktop/app/layout.tsx
@@ -0,0 +1,31 @@
+import "@repo/ui/styles.css";
+import "@repo/player/styles.css";
+
+import "./globals.css";
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Toaster } from "@repo/ui";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "TouchMe Music",
+  description: "TouchMe Music Player",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+
+        <Toaster />
+      </body>
+    </html>
+  );
+}
